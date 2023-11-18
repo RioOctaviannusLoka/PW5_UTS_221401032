79f4fe8c66442b7d32e61fd4c78b8d5719d691f8
Rio <rioloka86@gmail.com>
2023-11-18 16:19:13 +0700

    Memperbarui tampilan (css)

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function showHiddenCard() {
       hiddenCard.style.display = 'none';
     }
   }
+
+  // Fungsi untuk memeriksa apakah elemen dalam viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Fungsi untuk menampilkan efek fade-in saat elemen dalam viewport
+function showFadeIn() {
+  const elements = document.querySelectorAll('.fade-in');
+  elements.forEach(element => {
+    if (isInViewport(element)) {
+      element.classList.add('active');
+    }
+  });
+}
+
+// Memanggil fungsi showFadeIn saat discroll atau saat halaman dimuat
+window.addEventListener('scroll', showFadeIn);
+window.addEventListener('load', showFadeIn);
